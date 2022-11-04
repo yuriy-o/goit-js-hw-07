0ba9a8448470725287a1262e54ce27000e43abb8
Yuriy <sharik.kiev@gmail.com>
2022-11-05 00:14:05 +0200

    start

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,6 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+console.log(galleryEl);
