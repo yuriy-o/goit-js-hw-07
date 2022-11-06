796c0467d5b605b869f0150a03d8025adc2ab92a
Yuriy <sharik.kiev@gmail.com>
2022-11-06 23:35:42 +0200

    add another close symbol

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -21,23 +21,9 @@ function onCreateImagesGallery(galleryItems) {
     .join('');
 }
 
-// imageGalleryEl.addEventListener('click', onModalOpen);
-
-// function onModalOpen(e) {
-//   e.preventDefault();
-
-//   console.log('e', e);
-//   console.log('e.target', e.target);
-//   console.log('e.path', e.path);
-//   console.log('e.path', e.target.classList.contains('gallery__image'));
-// }
-
 let gallery = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
   overlayOpacity: 0.8,
+  closeText: '☣',
 });
-gallery.on('show.simplelightbox', function () {
-  // do something…
-});
-
